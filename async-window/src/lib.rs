722f6e989d6397d4ca7b14087915b7317461e667
Lukas Bombach <lukas.bombach@googlemail.com>
2021-05-19 20:55:40 +0200

    opening winit window in async-window project

--- async-window/src/lib.rs
+++ async-window/src/lib.rs
@@ -1,7 +1,27 @@
 use neon::prelude::*;
 
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node"))
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
 }
 
 #[neon::main]
